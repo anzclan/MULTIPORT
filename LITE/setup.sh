33e1d7fd5465d192a78ed84e1b0c5dfc562417c6
aNz <107313945+anzclan@users.noreply.github.com>
2022-09-30 17:05:38 +0800

    Update setup.sh

--- LITE/setup.sh
+++ LITE/setup.sh
@@ -11,6 +11,24 @@ exit 1
 fi
 
 mkdir /etc/xray;
+read -p " Hostname / Domain: " host
+
+echo "$host" >> /etc/xray/domain
+echo "$host" >> ./domain
+
+domain=$(cat /etc/xray/domain)
+sudo lsof -t -i tcp:80 -s tcp:listen | sudo xargs kill
+cd /root/
+wget -O acme.sh https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh
+bash acme.sh --install
+rm acme.sh
+cd .acme.sh
+echo "Starting...., Port 80 will be stopped during the Cert install process"
+bash acme.sh --register-account -m nor4ini96@gmail.com
+bash acme.sh --issue --standalone -d $domain --force
+bash acme.sh --installcert -d $domain --fullchainpath /etc/xray/xray.crt --keypath /etc/xray/xray.key
+clear
+
 wget https://raw.githubusercontent.com/anzclan/MULTIPORT/main/LITE/nginx.sh && chmod +x nginx.sh && ./nginx.sh
 wget https://raw.githubusercontent.com/anzclan/MULTIPORT/main/LITE/XRAY/install-xray.sh && chmod +x install-xray.sh && ./install-xray.sh
 
