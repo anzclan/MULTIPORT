2b88a7e951d1f90a399f197c76d65caeb6fdaef0
aNz <107313945+anzclan@users.noreply.github.com>
2022-10-02 12:33:25 +0800

    Update nginx.sh

--- LITE/nginx.sh
+++ LITE/nginx.sh
@@ -5,8 +5,8 @@
 apt -y install nginx php php-fpm php-cli php-mysql libxml-parser-perl
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-curl https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/ssh/nginx.conf > /etc/nginx/nginx.conf
-curl https://raw.githubusercontent.com/fisabiliyusri/Mantap/main/ssh/vps.conf > /etc/nginx/conf.d/vps.conf
+curl https://raw.githubusercontent.com/anzclan/MULTIPORT/main/LITE/NGINX/nginx.conf > /etc/nginx/nginx.conf
+curl https://raw.githubusercontent.com/anzclan/MULTIPORT/main/LITE/NGINX/vps.conf > /etc/nginx/conf.d/vps.conf
 sed -i 's/listen = \/var\/run\/php-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/fpm/pool.d/www.conf
 useradd -m vps;
 mkdir -p /home/vps/public_html
@@ -14,7 +14,7 @@ echo "<?php phpinfo() ?>" > /home/vps/public_html/info.php
 chown -R www-data:www-data /home/vps/public_html
 chmod -R g+rw /home/vps/public_html
 cd /home/vps/public_html
-wget -O /home/vps/public_html/index.html "https://${akbarvpn}/index.html1"
+wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/anzclan/addon/main/multiport"
 /etc/init.d/nginx restart
 ##
 ## Hapus
